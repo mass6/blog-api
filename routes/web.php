8fdecc8a3034658512e54fc16ae81467eb3e0cc8
Sam <samcharmy@gmail.com>
2016-11-13 15:38:12 +0100

    Add web route to fetch oath token for testing.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use App\User;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +20,30 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+
+// Test route for fecthing a test token
+Route::get('request-token/{id?}', function($id = null) {
+    $PasswordGrantClient = DB::table('oauth_clients')->where('name', 'Laravel Password Grant Client')->get()->first();
+
+    if (! is_null($id)) {
+        $user = User::find($id);
+    }
+    else {
+        $user = User::first();
+    }
+
+    $http = new GuzzleHttp\Client;
+    $response = $http->post('http://blog-api.app/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'password',
+            'client_id' => $PasswordGrantClient->id,
+            'client_secret' => $PasswordGrantClient->secret,
+            'username' => $user->email,
+            'password' => 'secret',
+            'scope' => '',
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
